79cb42644a2425c237e007c0f05d7034b7a3c20f
rutra8002 <arturkummer08@gmail.com>
2023-03-28 12:48:08 +0200

    fe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
-from pygame.locals import *
+
+from pygame import QUIT, KEYDOWN, K_LEFT, K_RIGHT, K_UP, K_DOWN, KEYUP
 
 pygame.init()
 
@@ -20,6 +21,24 @@ pygame.display.set_caption("Game")
 # text = text_img.get_rect()
 # text.x = 100
 # text.y = 550
+class Player:
+    def __init__(self, playerID, x, y):
+        self.player_img = pygame.image.load("egg.png")
+        self.player = self.player_img.get_rect()
+        #self.player_img = pygame.transform.scale(self.player_img, (100, 100))
+        self.playerID = playerID
+        self.xSpeed = 0
+        self.ySpeed = 0
+        self.x = x
+        self.y = y
+
+    def draw(self, x, y):
+        self.player = self.player_img.get_rect()
+        screen.blit(self.player_img, (x, y))
+
+player2 = Player(1, 300, 400)
+
+
 
 running = True
 while running:
@@ -30,10 +49,22 @@ while running:
             running = False
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                print("left")
+                player2.xSpeed = -1
             if event.key == K_RIGHT:
-                print("right")
+                player2.xSpeed = 1
+            if event.key == K_UP:
+                player2.ySpeed = -1
+            if event.key == K_DOWN:
+                player2.ySpeed = 1
+        if event.type == KEYUP:
+            if event.key == K_LEFT or event.key == K_RIGHT:
+                player2.xSpeed = 0
+            if event.key == K_UP or event.key == K_DOWN:
+                player2.ySpeed = 0
 
+    player2.draw(player2.x + player2.xSpeed, player2.y + player2.ySpeed)
+    player2.x += player2.xSpeed
+    player2.y += player2.ySpeed
     pygame.display.update()
 
 pygame.quit()
