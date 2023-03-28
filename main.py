a992ee38d54ffc7a35cd05bcaf8ad49e8de23b59
rutra8002 <arturkummer08@gmail.com>
2023-03-28 13:27:34 +0200

    fe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-from pygame import QUIT, KEYDOWN, K_LEFT, K_RIGHT, K_UP, K_DOWN, KEYUP
+from pygame import QUIT, KEYDOWN, K_LEFT, K_RIGHT, K_UP, K_DOWN, KEYUP, K_s, K_w, K_d, K_a
 
 pygame.init()
 
@@ -35,36 +35,57 @@ class Player:
     def draw(self, x, y):
         self.player = self.player_img.get_rect()
         screen.blit(self.player_img, (x, y))
+    def points(self):
+        points = 0
+        points += 1
+        return points
+class Jajko:
+    def __init__(self):
+        self.player_img = pygame.image.load("egg.png")
+        self.player = self.player_img.get_rect()
+    def draw(self):
+        self.player = self.player_img.get_rect()
+        screen.blit(self.player_img, (random.randint(1, 800), random.randint(1, 600)))
+
+
+
+
 
 player2 = Player(1, 300, 400)
 
+pygame.font.init() # you have to call this at the start,
+                   # if you want to use this module.
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 
 running = True
 while running:
+
     screen.fill((89, 179, 66)) #RGB
 
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
         if event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                player2.xSpeed = -1
-            if event.key == K_RIGHT:
-                player2.xSpeed = 1
-            if event.key == K_UP:
-                player2.ySpeed = -1
-            if event.key == K_DOWN:
-                player2.ySpeed = 1
+            if event.key == K_a:
+                player2.xSpeed = -0.2
+            if event.key == K_d:
+                player2.xSpeed = 0.2
+            if event.key == K_w:
+                player2.ySpeed = -0.2
+            if event.key == K_s:
+                player2.ySpeed = 0.2
         if event.type == KEYUP:
-            if event.key == K_LEFT or event.key == K_RIGHT:
+            if event.key == K_a or event.key == K_d:
                 player2.xSpeed = 0
-            if event.key == K_UP or event.key == K_DOWN:
+            if event.key == K_w or event.key == K_s:
                 player2.ySpeed = 0
+
 
     player2.draw(player2.x + player2.xSpeed, player2.y + player2.ySpeed)
     player2.x += player2.xSpeed
     player2.y += player2.ySpeed
+    screen.blit(my_font.render('Points: ' + str(player2.points()), False, (0, 0, 0)), (0, 0))
     pygame.display.update()
 
 pygame.quit()
